ec6c7745403689e0d146b7e914a75c230f200a3b
Juliusmwash <cutejuliusmwash@gmail.com>
2023-10-31 17:07:28 +0300

    Commit files for the third task

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -23,12 +23,22 @@ class Config:
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    """
+    Use request.accept_languages to determine the best
+    match with supported languages.
+    """
+    return request.accept_languages.best_match(
+            app.config['LANGUAGES'])
+
+
 @app.route('/')
-def root():
+def root() -> str:
     """
     Renders 'Hello world' when a request is made to this route.
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
